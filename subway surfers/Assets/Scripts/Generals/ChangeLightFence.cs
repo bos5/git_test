fff5f2996bfe1f197139ed8e5debd33a88dd9c30
bos5 <duong552002@gmail.com>
2023-02-18 01:16:17 +0700

    up file code

---
+++ subway surfers/Assets/Scripts/Generals/ChangeLightFence.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+// thay đổi màu sắc của dải mũi tên trên thanh đi lên xe 
+public class ChangeLightFence : MonoBehaviour
+{
+    public Material lightRed, lightGreen, lightYellow;
+    public Vector2 timeChange = new Vector2(5, 10);//tinh bang giay
+    public bool lightRedNow = true;
+    private bool lightYellowNow = false;
+    private MeshRenderer myMesh;
+
+    void Start()
+    {
+        lightRedNow = true;
+        myMesh = transform.GetComponent<MeshRenderer>();
+        Invoke("UpdateNewLight", Random.Range(timeChange.x, timeChange.y));
+    }
+
+    void UpdateNewLight()
+    {
+        if (lightRedNow)
+        {
+            myMesh.material = lightGreen;
+            lightRedNow = false;
+            lightYellowNow = false;
+        }
+        else
+        {
+            if (lightYellow != null)
+            {
+                if (lightYellowNow)
+                {
+                    myMesh.material = lightRed;
+                    lightRedNow = true;
+                }
+                else
+                {
+                    myMesh.material = lightYellow;
+                    lightYellowNow = true;
+                }
+            }
+            else
+            {
+                myMesh.material = lightRed;
+                lightRedNow = true;
+            }
+        }
+        Invoke("UpdateNewLight", Random.Range(timeChange.x, timeChange.y));
+    }
+}
